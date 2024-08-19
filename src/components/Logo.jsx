153caff82a26eac3b1fd851a4057462a3c570622
gfa789 <georgeatki1@gmail.com>
2024-08-20 00:06:16 +0100

    Made timeline and logo

---
+++ src/components/Logo.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import './Logo.css'
+
+function Logo() {
+    return (
+        <div className="logo-container">
+      <div className="logo-content">
+        <div className="name first-name">
+          g
+        </div>
+        <div className="name last-name">
+          A
+        </div>
+      </div>
+    </div>
+    );
+}
+
+export default Logo;
---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+import './Navbar.css'
+import Logo from "../Logo";
+
+function Navbar() {
+
+
+    return(
+        <div className="navbar-container">
+            <Logo />
+
+            <div className="navbar-items">
+                <div className="navbar-links">
+
+                </div>
+                <div className="navbar-menu-button">
+
+                </div>
+            </div>
+            
+        </div>
+    )
+}
+
+export default Navbar
