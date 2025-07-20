3aa92847166ede636aa714cde75c1c8642ec9bb4
George <98461385+gfa789@users.noreply.github.com>
2025-07-20 16:59:44 +0100

    Update Header.jsx
    Reviewed by Jack

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -144,7 +144,7 @@ const Header = () => {
             animate={{ opacity: 1, y: 0 }}
             transition={{ duration: 0.5, delay: 0.2 }}
           >
-            Software Engineer, 21
+            Software Engineer, 22
           </motion.h2>
           <motion.p 
             className="text-lg max-w-md text-gray-200"
